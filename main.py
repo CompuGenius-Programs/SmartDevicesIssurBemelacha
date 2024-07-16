834bc6f8fbe0e75ce00585677558b3f778c2aad1
CompuGenius <contact@cgprograms.com>
2024-07-16 18:33:53 -0400

    Moved IPs and sleep time to config file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
+import json
 import logging
 import os
 from datetime import datetime
 
+import kasa.exceptions
 import pytz
 from dotenv import load_dotenv
 from kasa import Discover, Device
@@ -11,7 +13,6 @@ from zmanim.zmanim_calendar import ZmanimCalendar
 
 load_dotenv()
 
-device_ip = os.getenv("DEVICE_IP")
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 
@@ -23,28 +24,45 @@ longitude = float(os.getenv("LONGITUDE"))
 location = GeoLocation(location, latitude, longitude, timezone, elevation=15)
 calendar = ZmanimCalendar(geo_location=location)
 
-sleep_mins = 30
 logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', level=logging.INFO,
-                    datefmt='%a %b %d - %I:%M %p')
+                    datefmt='%a %b %d - %I:%M:%S %p')
+
+
+async def connect_devices(devices_ips):
+    logging.info(f"Devices IPs: {devices_ips}")
+    configs = []
+
+    for device_ip in devices_ips:
+        try:
+            device = await Discover.discover_single(device_ip, username=username, password=password)
+        except kasa.exceptions.KasaException:
+            logging.error(f"{device_ip} | Could not connect to device")
+            continue
+        configs.append(device.config.to_dict())
+        await device.update()
+        logging.info(f"{device.alias} | Connected to device")
+
+    return configs
 
 
 async def main():
-    device = await Discover.discover_single(device_ip, username=username, password=password)
-    config_dict = device.config.to_dict()
-    await device.update()
-    logging.info(f"Connected to device: {device.alias}")
-
     while True:
-        now = datetime.now(pytz.timezone(timezone))
-        if calendar.is_assur_bemelacha(now):
-            dev = await Device.connect(config=Device.Config.from_dict(config_dict))
-            if not dev.is_on:
-                await dev.turn_on()
-                logging.info("Turned light on.")
-            else:
-                logging.info("Light is already on.")
+        with open("config.json", "r") as f:
+            config = json.load(f)
 
-        await asyncio.sleep(60 * sleep_mins)
+        now = datetime.now(pytz.timezone(timezone))
+        if not calendar.is_assur_bemelacha(now):
+            device_configs = await connect_devices(config["devices_ips"])
+            for dev_config in device_configs:
+                device = await Device.connect(config=Device.Config.from_dict(dev_config))
+                if not device.is_on:
+                    await device.turn_on()
+                    logging.info(f"{device.alias} | Turned light on")
+                else:
+                    logging.info(f"{device.alias} | Light is already on")
+
+        sleep_time = config["sleep_time"]
+        await asyncio.sleep(sleep_time * 60)
 
 
 if __name__ == "__main__":
